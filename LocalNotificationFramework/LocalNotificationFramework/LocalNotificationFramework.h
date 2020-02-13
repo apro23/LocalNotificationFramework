6ea9f299587d1a9d4e759660366badebf921acbb
Argyl P. Rebanal <argylrebanal@gmail.com>
2020-02-13 16:51:59 +0800

    initial commit

---
+++ LocalNotificationFramework/LocalNotificationFramework/LocalNotificationFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocalNotificationFramework.h
+//  LocalNotificationFramework
+//
+//  Created by Argyl P. Rebanal on 2/13/20.
+//  Copyright © 2020 Argyl P. Rebanal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalNotificationFramework.
+FOUNDATION_EXPORT double LocalNotificationFrameworkVersionNumber;
+
+//! Project version string for LocalNotificationFramework.
+FOUNDATION_EXPORT const unsigned char LocalNotificationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalNotificationFramework/PublicHeader.h>
+
+
